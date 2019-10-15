40daf7872b7c8be43dd3f897aaf96e9f5fd12ac2
Hubert Cieslinski <hubert.cieslinskii@gmail.com>
2019-10-15 23:57:32 +0200

    AddCamera

---
+++ Assets/Script/Camera/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] float followSpeed = 1f;
+    [SerializeField] Transform target;
+
+    private void Update()
+    {
+        Vector3 newPos = target.position;
+        newPos.z = -10f;
+        transform.position = Vector3.Slerp(transform.position, newPos, followSpeed);
+    }
+}
