8ec7f4783372f9fdbcd32cde71450031be58de27
Hubert Cieslinski <hubert.cieslinskii@gmail.com>
2019-10-18 12:55:53 +0200

    Generation Map

--- Assets/RoomSpawner.cs
+++ Assets/RoomSpawner.cs
@@ -6,8 +6,57 @@ public class RoomSpawner : MonoBehaviour
 {
     [SerializeField] Constant.Direction NeedOpenDoor;
 
+    private RoomTemplate templates;
+    
+    private int rand = 0;
+    private bool isSpawned = false;
+
+    public void Start()
+    {
+        templates = FindObjectOfType<RoomTemplate>();
+        Invoke("SpawnRoom", 1f);
+    }
+
     private void SpawnRoom()
     {
-         
+        if(isSpawned == false)
+        {
+            if(NeedOpenDoor == Constant.Direction.Bottom)
+            {
+            // Need to spawn a room with a BOTTOM door.
+                rand = Random.Range(0, templates.bottomRooms.Length);
+                Instantiate(templates.bottomRooms[rand], transform.position, templates.bottomRooms[rand].transform.rotation);
+            } 
+            else if(NeedOpenDoor == Constant.Direction.Top)
+            {
+                // Need to spawn a room with a TOP door.
+                rand = Random.Range(0, templates.topRooms.Length);
+                Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
+                
+            }
+            else if(NeedOpenDoor == Constant.Direction.Left)
+            {
+                // Need to spawn a room with a LEFT door.
+                rand = Random.Range(0, templates.leftRooms.Length);
+                Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
+            } 
+            else if(NeedOpenDoor == Constant.Direction.Right)
+            {
+                // Need to spawn a room with a RIGHT door.
+                rand = Random.Range(0, templates.rightRooms.Length);
+                Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
+            }
+            isSpawned = true;
+        }
     }
+
+    void OnTriggerEnter2D(Collider2D other){
+		if(other.CompareTag("SpawnPoint")){
+			if(other.GetComponent<RoomSpawner>().isSpawned == true && isSpawned == false){
+				Destroy(gameObject);
+			} 
+			isSpawned = true;
+		}
+	}
+
 }
